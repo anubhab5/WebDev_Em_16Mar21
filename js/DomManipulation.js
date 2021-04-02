d11a443c86f09c4191e2216263c27cf586198c6a
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2021-04-02 11:26:29 +0530

    JS- DOM MANIPULATION

---
+++ js/DomManipulation.js
@@ -0,0 +1,27 @@
+
+
+// Selecting element
+// const heading = document.getElementById("heading");
+// heading.innerText = "some manipulated data";
+
+// const heading = document.getElementsByClassName("my-class")[1];
+
+// const heading = document.getElementsByTagName("h3");
+
+// const heading = document.getElementsByName("tag");
+
+
+// CREATE AN ELEMENT
+// const headingH1 = document.createElement("h1");
+// headingH1.innerText = "SOme data";
+// document.body.appendChild(headingH1);
+
+
+const heading = document.getElementById("heading");
+heading.innerText = "some manipulated data";
+// heading.style.color  = "red";
+// heading.style.background  = "green";
+debugger
+heading.classList.add("color-me");
+heading.classList.add("asas");
+debugger
---
+++ js/eventJS.js
@@ -0,0 +1,19 @@
+function callMe() {
+  const userName = document.getElementById("user-name");
+  const userNameValue = userName.value;
+
+  const newElement = document.createElement("h2");
+  newElement.innerText = userNameValue;
+  document.body.appendChild(newElement);
+
+  //   if (val1 !== val2) {
+  //   }
+
+  debugger;
+}
+
+function focussed() {
+  
+    debugger
+    
+}
